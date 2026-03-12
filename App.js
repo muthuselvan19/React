30e95da60303fb881a3097201344633c228992bd
muthuselvan19 <selvammuthu84972@gmail.com>
2026-03-12 18:43:05 +0530

    Component are's added as local and reusable code built

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
-
+import logo from './assets/ResLogo.png'
+console.log(logo);
 const Heading = () => {
     return (
         <div className="head">
-            <img className="headLogo" src="https://imgs.search.brave.com/cGMt3cU-_D6UBLYV8tAv1B3busPWpbuGEWBxqzuVEuE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tYXJr/ZXRwbGFjZS5jYW52/YS5jb20vRUFHSHp1/RUZwaHcvMi8wLzE2/MDB3L2NhbnZhLWJs/YWNrLWFuZC13aGl0/ZS1taW5pbWFsaXN0/LWNpcmNsZS1yZXN0/YXVyYW50LWZvb2Qt/bG9nby1WSmFhTlNt/ZElrYy5qcGc" alt="image not Found"></img>
+            <img className="headLogo" src={logo} alt="image not Found"></img>
             <h2 className="resName">Bailey Restaurent</h2>
             <ul className="tagList">
                 <li>Home</li>
@@ -18,17 +19,136 @@ const Heading = () => {
 const Body = () => {
     return (
         <div className="bodyContent">
-            
+            <div className="searchBar">
+                <input type="text" className="searcher" placeholder="search"></input>
+                <input type="submit" className="submitArrow"></input>
+            </div>
+             {restaurentData.map((restaurant) => (
+          <Block key={restaurant.id} resData={restaurant} />
+        ))}
+
+        </div>
+    );
+}
+
+
+export const restaurentData = [
+    {
+        id: 1,
+        imgSrc:
+            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/12/5/dc74a4b7-8d63-4eb7-91c9-048973c2cc99_1003414.jpg",
+        headingText: "Devine Hotel",
+        ratingData: "4.5",
+        deleveryTime: "• 25-30 min",
+        para1: "Mecxican Spicy Style",
+        para2: "Near Kumbakonam",
+    },
+    {
+        id: 2,
+        imgSrc:
+            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/gp1ityra6utvzqn6ghnv",
+        headingText: "A2B",
+        ratingData: "3.9",
+        deleveryTime: "• 20-30 min",
+        para1: "Biryani, suttle Style",
+        para2: "Near Papanasam",
+    },
+    {
+        id: 3,
+        imgSrc:
+            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/5/26/78517964-20dc-4e37-92ce-8990deb14ab6_898041.jpg",
+        headingText: "Meat & Spice",
+        ratingData: "4.2",
+        deleveryTime: "• 25-30 min",
+        para1: "Tandoori, Snacks , Entertinment",
+        para2: "Kaveri Nagar",
+    },
+    {
+        id: 4,
+        imgSrc:
+            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/rvxp5xbniat84r6efku2",
+        headingText: "Mixed Dhaba",
+        ratingData: "4.0 • 28-33 min",
+        para1: "Punjabi Style , Western Style",
+        para2: "Mumbai",
+    },
+    {
+        id: 5,
+        imgSrc:
+            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_112,h_112,c_fill/Autosuggest/Top%20200%20queries/Burger.png",
+        headingText: "Burger Hub",
+        ratingData: "4.1",
+        deleveryTime: "• 25-30 min",
+        para1: "Fast Food , French Fries",
+        para2: "Chennai",
+    },
+    {
+        id: 6,
+        imgSrc:
+            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/sidigb8zqjfrfpkrtqgl",
+        headingText: "Seank & Snack",
+        ratingData: "4.3",
+        deleveryTime: "• 25-30 min",
+        para1: "Snacks, Tea , Hot Items",
+        para2: "Vendiyur",
+    },
+    {
+        id: 7,
+        imgSrc:
+            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/dd519572ed1bed19de47e94186e743ea",
+        headingText: "The Wrap Hub",
+        ratingData: "3.8",
+        deleveryTime: "• 25-30 min",
+        para1: "Wraps, Rolls",
+        para2: "Factory Road",
+    },
+    {
+        id: 8,
+        imgSrc:
+            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/xph0vf5pvbxjf8xchwjk",
+        headingText: "Dhaba Hub",
+        ratingData: "4.6",
+        deleveryTime: "• 25-30 min",
+        para1: "Dhaba",
+        para2: "Sivaji Nagar",
+    },
+    {
+        id: 9,
+        imgSrc:
+            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/uxgpy3bwmc5rsojmtw43",
+        headingText: "Cafe Hawana",
+        ratingData: "4.4",
+        deleveryTime: "• 25-30 min",
+        para1: "Tea , coffee , shawarma",
+        para2: "Near 4-Road",
+    },
+];
+
+const Block = (props) => {
+    const { resData } = props;
+    if (!resData) {
+        console.log("Wrong");
+        return null;
+    }
+    return (
+        <div className="item">
+            <img src={resData.imgSrc}/>
+            <h3>{resData.headingText}</h3>
+            <p>{resData.ratingData}</p>
+            <i>{resData.deleveryTime}</i>
+            <p>{resData.para1}</p>
+            <p>{resData.para2}</p>
+
         </div>
     );
 }
 
 const App = () => {
     return (
-    <>
-    <Heading/>
-    <Body/>
-    </>);
+        <>
+            <Heading />
+            <Body />
+        </>);
 }
 
 let creationRoot = ReactDOM.createRoot(document.getElementById("root"));
